4202fa44a0c70630b52c060c8cd65dc258d8824e
Matthew Bristing <matthew@bristing.com>
2024-02-23 12:23:56 -0800

    Added Deque class and removed old identity printing modules in Stack and Queue

--- atds.py
+++ atds.py
@@ -5,9 +5,6 @@ class Stack(object):
     def __init__(self):
         # creates the stack (bottom = stack[0], top = stack[-1])
         self.stk = []
-
-    #def prlis(self):
-    #    print(self.stk[0:])
     
     def __repr__(self):
         return str(self.stk)
@@ -41,10 +38,7 @@ class Stack(object):
 class Queue(object):
     def __init__(self):
         self.q = []
-    
-    #def prlis(self):
-    #    print(self.q[0:])
-    
+        
     def __repr__(self):
         return str(self.q)
 
@@ -68,3 +62,34 @@ class Queue(object):
     
     def is_empty(self):
         return len(self.q) == 0
+
+class Deque(object):
+    def __init__(self):
+        self.d = []
+    
+    def __repr__(self):
+        return str(self.d)
+
+    def add_front(self, item):
+        return self.d.insert(0, item)
+
+    def add_rear(self, item):
+        return self.d.append(item)
+    
+    def remove_front(self):
+        if len(self.d) > 0:
+            return self.d.pop(0)
+        else:
+            return None
+        
+    def remove_rear(self):
+        if len(self.d) > 0:
+            return self.d.pop(-1)
+        else:
+            return None
+    
+    def size(self):
+        return len(self.d)
+    
+    def is_empty(self):
+        return len(self.d) == 0
